ff10e91413b7199a75456092a76917e25d74b3bf
Angelo <kelvinmbuguaw@gmail.com>
2020-08-08 14:12:15 +0300

    modified postdetail.html

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -80,10 +80,15 @@ WSGI_APPLICATION = 'restaurant.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mkahawa',
+        'USER': 'postgres',
+        'PASSWORD':'securedd@2',
+        'HOST':'localhost',
+        'PORT':'8000',
     }
 }
+
 
 
 # Password validation
